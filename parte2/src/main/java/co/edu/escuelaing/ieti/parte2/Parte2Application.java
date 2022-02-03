d83cc3580aaa402f4d0c4e8a1029daad314a9883
LeidyDucuara <leidy.ducuara@mail.escuelaing.edu.co>
2022-02-03 12:34:50 -0500

    commit inicial parte2

---
+++ parte2/src/main/java/co/edu/escuelaing/ieti/parte2/Parte2Application.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.ieti.parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parte2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parte2Application.class, args);
+	}
+
+}
